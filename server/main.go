451396a6682fb8ad85b6761b921b0d68e5509d88
teo <ferizovic.teo@gmail.com>
2022-10-30 19:01:55 +0100

    Server Streaming RPC

--- server/main.go
+++ server/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"context"
 	"log"
-
-	//"math/rand"
 	"net"
+	"time"
 
 	userpb "github.com/teo/sirius/proto"
+	wearablepb "github.com/teo/sirius/proto2"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 const (
@@ -37,8 +39,31 @@ func main() {
 
 	grpcServer := grpc.NewServer()
 	userServer := &userService{}
+	wearableServer := &wearableService{}
+
+	healthServer := health.NewServer()
+
+	go func() {
+		for {
+			status := healthpb.HealthCheckResponse_SERVING
+			// Check if user Service is valid
+			if time.Now().Second()%2 == 0 {
+				status = healthpb.HealthCheckResponse_NOT_SERVING
+			}
+
+			healthServer.SetServingStatus(userpb.UserService_ServiceDesc.ServiceName, status)
+			healthServer.SetServingStatus("", status)
+
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	healthServer.SetServingStatus("", healthpb.HealthCheckResponse_SERVING)
+	healthServer.SetServingStatus(userpb.UserService_ServiceDesc.ServiceName, healthpb.HealthCheckResponse_SERVING)
 
 	userpb.RegisterUserServiceServer(grpcServer, userServer)
+	healthpb.RegisterHealthServer(grpcServer, healthServer)
+	wearablepb.RegisterWearableServiceServer(grpcServer, wearableServer)
 
 	grpcServer.Serve(lis)
 }
